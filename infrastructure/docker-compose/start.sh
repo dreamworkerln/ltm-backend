9ce9a2169eae0047067b5a9efeb6b3f2f13007ad
hatuncev <hatuncev.ku@kvant-telecom.ru>
2020-09-25 21:46:24 +0300

    working with docker-compose settings

--- infrastructure/docker-compose/start.sh
+++ infrastructure/docker-compose/start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -o allexport
-source config
+
+# allow use bash params as env to pass them to docker-composer to further substitution in docker-compose.yml
+set -a
+#set -o allexport
+
+source ./config
 docker-compose -p ${PROJECT_NAME} up -d
+#docker-compose -p ${PROJECT_NAME} up
--- utils/src/main/java/ru/geekbrains/handmade/ltmbackend/utils/Junit5Extension.java
+++ utils/src/main/java/ru/geekbrains/handmade/ltmbackend/utils/Junit5Extension.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 
 import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
 
+// https://stackoverflow.com/questions/43282798/in-junit-5-how-to-run-code-before-all-tests/51556718#51556718
+// In JUnit 5, how to run code before all tests
 public class Junit5Extension implements BeforeAllCallback, ExtensionContext.Store.CloseableResource, AfterAllCallback {
 
     private static boolean started = false;
+
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
@@ -23,7 +26,7 @@ public class Junit5Extension implements BeforeAllCallback, ExtensionContext.Stor
             started = true;
             // Your "before all tests" startup logic goes here
             // The following line registers a callback hook when the root test context is shut down
-            context.getRoot().getStore(GLOBAL).put("CMS_JUNIT_DOCKER_STARTER", this);
+            context.getRoot().getStore(GLOBAL).put("LTM_JUNIT_DOCKER_STARTER", this);
         }
 
     }
